14747d06cc3792aff398af71e717c7ac14cdf953
Marek Libra <marek.libra@gmail.com>
2022-04-03 15:36:56 +0200

    MGMT-9263: Add GPU Dashboard - register
    Initial commit.

---
+++ src/components/GPUDashboard.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+/* Chaotic list of TODOs:
+     - Add "New" badge in gpu-dashboard nav item - replace string by a component in console-extensions.json
+*/
+
+const GPUDashboard: React.FC = () => {
+  return <div>Hello from the GPUDashboard</div>;
+};
+
+export default GPUDashboard;
