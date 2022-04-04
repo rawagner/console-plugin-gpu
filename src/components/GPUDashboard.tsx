6cc1d8b592b0725c4f69e5d91fe58a0646f72b10
Marek Libra <marek.libra@gmail.com>
2022-04-04 14:10:03 +0200

    Add basic structure and first dummy chart

--- src/components/GPUDashboard.tsx
+++ src/components/GPUDashboard.tsx
@@ -1,3 +1,18 @@
+import {
+  ChartArea,
+  ChartDonutUtilization,
+  ChartGroup,
+  ChartVoronoiContainer,
+} from '@patternfly/react-charts';
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  Gallery,
+  Page,
+  PageSection,
+} from '@patternfly/react-core';
 import * as React from 'react';
 
 /* Chaotic list of TODOs:
@@ -7,14 +22,53 @@ import * as React from 'react';
 // https://issues.redhat.com/browse/MGMT-9263
 // https://miro.com/app/board/uXjVOeUB2B4=/?moveToWidget=3458764514332229879&cot=14
 
-// https://www.patternfly.org/v4/demos/card-view
-
-// https://www.patternfly.org/v4/components/card
-// https://www.patternfly.org/v4/charts/donut-utilization-chart
-// https://www.patternfly.org/v4/charts/sparkline-chart
-
 const GPUDashboard: React.FC = () => {
-  return <div>Hello from the GPUDashboard</div>;
+  return (
+    <Page>
+      <PageSection isFilled>
+        <Gallery>
+          <Card>
+            <CardTitle className="pf-u-text-align-center">GPU utilization</CardTitle>
+            <CardBody>
+              <ChartDonutUtilization
+                ariaDesc="GPU utilization"
+                ariaTitle="Donut GPU utilization"
+                constrainToVisibleArea={true}
+                data={{ x: 'Not used', y: 55 }}
+                labels={({ datum }) => (datum.x ? `GPU utilization: ${datum.y}%` : null)}
+                // subTitle=""
+                title="55%"
+              />
+            </CardBody>
+            <CardFooter>
+              <ChartGroup
+                ariaDesc="GPU utilization over time"
+                ariaTitle="Sparkline GPU utilization"
+                containerComponent={
+                  <ChartVoronoiContainer
+                    labels={({ datum }) => `GPU utilization at ${datum.x}: ${datum.y}`}
+                    constrainToVisibleArea
+                  />
+                }
+                height={70}
+                maxDomain={{ y: 100 }}
+                padding={0}
+              >
+                <ChartArea
+                  data={[
+                    { name: 'Utilization', x: '2015', y: 30 },
+                    { name: 'Utilization', x: '2016', y: 40 },
+                    { name: 'Utilization', x: '2017', y: 80 },
+                    { name: 'Utilization', x: '2018', y: 60 },
+                  ]}
+                />
+              </ChartGroup>
+            </CardFooter>
+          </Card>
+        </Gallery>
+      </PageSection>
+    </Page>
+  );
 };
 
 export default GPUDashboard;
