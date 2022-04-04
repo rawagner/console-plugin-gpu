d5890d51f1c57bda0ebdb2fc8a3490e09ff7fd5f
Marek Libra <marek.libra@gmail.com>
2022-04-04 12:29:46 +0200

    Introduce GPU_OPERATOR flag

--- src/components/GPUDashboard.tsx
+++ src/components/GPUDashboard.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react';
 
 /* Chaotic list of TODOs:
-     - Add "New" badge in gpu-dashboard nav item - replace string by a component in console-extensions.json
+  - Add "New" badge in gpu-dashboard nav item - replace string by a component in console-extensions.json
 */
+
+// https://issues.redhat.com/browse/MGMT-9263
+// https://miro.com/app/board/uXjVOeUB2B4=/?moveToWidget=3458764514332229879&cot=14
+
+// https://www.patternfly.org/v4/demos/card-view
+
+// https://www.patternfly.org/v4/components/card
+// https://www.patternfly.org/v4/charts/donut-utilization-chart
+// https://www.patternfly.org/v4/charts/sparkline-chart
 
 const GPUDashboard: React.FC = () => {
   return <div>Hello from the GPUDashboard</div>;
